8c290e0caf7a7e59f5c1ce6181ae292a96270235
Thomas Barrett <tbarrett@thomass-mbp.myfiosgateway.com>
2021-12-27 11:56:51 -0500

    Basic gui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import './App.css';
+import { useCallback, useState, useEffect } from 'react';
+import { Web3ReactProvider, useWeb3React } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import stealableContractAbi from "./abi/StealableContract.json";
+import { ethers } from 'ethers'
+export const injectedConnector = new InjectedConnector({ supportedChainIds: [0x61] });
+
+const Main = () => {
+  const { library, chainId, account, activate, active } = useWeb3React()
+  const [owner, setOwner] = useState(null)
+  const connect = useCallback(async () => {
+    await activate(injectedConnector)
+  })
+
+  useEffect(async () => {
+    if (active) {
+      const contract = new ethers.Contract("0xC9EF3209652efd9bEd191E3a6a9CD720Cb5Ee388", stealableContractAbi, library);
+      const o = await contract.owner();
+      setOwner(o);
+    }
+  }, [library, active]);
+
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      <div>Owner: {owner == null ? "Loading...": owner}</div>
+      {active ? (
+        <div>✅</div>
+      ) : (
+        <button type="button" onClick={connect}>
+          Connect
+        </button>
+      )}
+    </div>
+  )
+}
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Main></Main>
+    </Web3ReactProvider>
   );
 }
 
